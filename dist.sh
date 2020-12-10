5a518c8d3fe3fe500b33c620330d62094564173f
okay <okay@chalk>
2020-12-10 06:43:47 -0800

    [build] use `arm-linux-gnueabihf-strip` instead of `strip`

--- dist.sh
+++ dist.sh
@@ -1,7 +1,7 @@
 qmake
 make
 mkdir -p ./dist
-strip ./src/client/librm2fb_client.so.1.0.0
+arm-linux-gnueabihf-strip ./src/client/librm2fb_client.so.1.0.0
 cp ./src/xofb/librm2fb_xofb.so.1.0.0 ./dist
 cp ./src/server/librm2fb_server.so.1.0.0 ./dist
 cp ./src/client/librm2fb_client.so.1.0.0 ./dist
