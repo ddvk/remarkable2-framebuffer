3971521798ca40080f8829b0a70fcd14c0d9b48c
Mattéo Delabre <spam@delab.re>
2021-07-21 17:48:00 +0200

    Fix loader build

--- dist.sh
+++ dist.sh
@@ -5,4 +5,3 @@ arm-linux-gnueabihf-strip ./src/client/librm2fb_client.so.1.0.1
 cp ./src/xofb/librm2fb_xofb.so.1.0.1 ./dist
 cp ./src/server/librm2fb_server.so.1.0.1 ./dist
 cp ./src/client/librm2fb_client.so.1.0.1 ./dist
-cp ./src/offsets ./dist
