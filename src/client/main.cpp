99f5b75576fc331e85cf1d2463304045bd1b0611
Linus <linus@cosmos-ink.net>
2021-11-30 14:09:25 +0100

    Use 1e9 again

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -172,9 +172,9 @@ int ioctl(int fd, unsigned long request, char *ptr) {
       }
 
       timeout.tv_nsec += SEM_WAIT_TIMEOUT;
-      // Move overflow ns to secs (1000000000 = 1e9 = 1s in ns)
-      timeout.tv_sec += timeout.tv_nsec / 1000000000;
-      timeout.tv_nsec %= 1000000000;
+      // Move overflow ns to secs
+      timeout.tv_sec += timeout.tv_nsec / (uint64_t) 1e9;
+      timeout.tv_nsec %= (uint64_t) 1e9;
 
       sem_timedwait(sem, &timeout);
 
