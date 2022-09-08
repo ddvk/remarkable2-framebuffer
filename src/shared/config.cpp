7b1f933d42aa97b576c9cf1f1c75a207b356e3fd
Glenn McCarthy <glenn.mccarthy@gmail.com>
2022-09-09 09:40:17 +1200

    Added support for 2.14.3.1005

--- src/shared/config.cpp
+++ src/shared/config.cpp
@@ -289,6 +289,15 @@ create addr 0x4c2750
 shutdown addr 0x4c26e8
 wait addr 0x4c16f0
 getInstance addr 0x4b66b4
+
+!20220907143405
+version str 2.14.3.1005
+update addr 0x4bfb2c
+updateType str QRect
+create addr 0x4c2750
+shutdown addr 0x4c26e8
+wait addr 0x4c16f0
+getInstance addr 0x4b66b4
 )CONF";
 
 void read_config_file(
